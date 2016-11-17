4c2ef77ec0322cbc2420be97bc90be38fe47abff
Anushna Prakash <anushnap@uw.edu>
2016-11-17 15:13:53 -0800

    attempted exercise

--- ggplot/ggplot_exercise.r
+++ ggplot/ggplot_exercise.r
@@ -16,9 +16,28 @@ suicides <- suicides %>%
 #  Make a line plot of suicides by age
 # (year on the x axis, deaths on the y axis, different line for each age).
 # facet by sex.
+# basics
+bare <- ggplot(suicides)
 
+#adding plot aesthetics
+aesthetic <- ggplot(suicides, aes(x=year, y=deaths))
+
+#adding points
+scatter <- ggplot(suicides, aes(x=year, y= deaths)) + geom_point()
+
+#split means by color
+color.by.means <- ggplot(suicides, aes(x = year, y = deaths, color = means)) +
+  geom_point()
+
+#facet by state
+state.fact <- ggplot(suicides, aes(x = year, y = deaths, color = means)) + geom_point() + 
+  facet_wrap(~state, scales = "free")
+
+state_fact <- ggplot(suicides, aes(x = year, y = deaths, color = means)) +
+  geom_bar(aes(fill = means), stat = "identity") + facet_wrap(~state, scales = "free")
 
 ##extra credit####
+
 
 one_state <- all_suicides[all_suicides$state=="Uttar Pradesh"] %>% 
   group_by(year, state, sex, age, means) %>% 
